47d0895af10bc1449590251149112dc79b1dbeef
Anachuri <alejandro.anachuri@dxc.com>
2022-03-11 13:04:53 -0300

    test config

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi_mqtt import FastMQTT, MQQTConfig
-#import logging
 import ssl
-
+from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
+temp = os.environ.get('STACKHERO_MOSQUITTO_HOST')
 
+'''
 sslSettings = ssl.SSLContext(ssl.PROTOCOL_TLS);
 sslSettings.verify_mode = ssl.CERT_NONE;
+
 
 mqtt_config = MQQTConfig(
     host = "5a4a5fcb82764eae8f9e5db02b9f707c.s1.eu.hivemq.cloud",
@@ -25,15 +29,18 @@ mqtt = FastMQTT(
 
 mqtt.init_app(app)
 
+temp = 0
 
 @mqtt.on_connect()
 def connect(client, flags, rc, properties):
-    mqtt.client.subscribe("/mqtt") #subscribing mqtt topic 
+    mqtt.client.subscribe("mqtt/temp") #subscribing mqtt topic 
     print("Connected: ", client, flags, rc, properties)
 
 @mqtt.on_message()
 async def message(client, topic, payload, qos, properties):
     print("Received message: ",topic, payload.decode(), qos, properties)
+    global temp
+    temp = payload.decode()
     return 0
 
 @mqtt.on_disconnect()
@@ -44,10 +51,8 @@ def disconnect(client, packet, exc=None):
 def subscribe(client, mid, qos, properties):
     print("subscribed", client, mid, qos, properties)
 
-
+'''
 
 @app.get("/")
-async def func():
-    #mqtt.publish("/mqtt", "Hello from Fastapi") #publishing mqtt topic 
-
-    return {"result": True,"message":"Published" }
+async def func(request: Request):
+    return templates.TemplateResponse("index.html",{"request": request, "temp":temp})
